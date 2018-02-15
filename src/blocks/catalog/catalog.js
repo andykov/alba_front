6287ac6814f7aad0a39dd4ec900ce8dd5dee1c61
Unknown <skaya.online@gmail.com>
2018-02-16 01:09:37 +0200

    fixed banner height + product price

--- src/blocks/catalog/catalog.js
+++ src/blocks/catalog/catalog.js
@@ -1,5 +1,19 @@
 // .catalog scripts goes here 
 
-/*$(function() {
+$(function() {
+
+	function bunnerSize() {
+		var catalogBanner = $('.catalog__item--banner .banner'),
+				catalogItem = $('.catalog__item'),
+				itemHeight = catalogItem.outerHeight(),
+				itemGap = catalogItem.css('margin-bottom').replace('px',''),
+				bannerHeight = +itemGap + 2 * itemHeight + 'px';
+
+		catalogBanner.css({'padding-bottom': bannerHeight});
+	}
+
+	$(window).on('load resize', function () {
+		bunnerSize();
+	})
 	
-});*/
+});

0c91881a1cf4996fd2558fa493eb534b30112fd2
pashkes <pashkes@gmail.com>
2018-02-19 17:44:26 +0200

    fixed indent page card

--- src/blocks/checkout/checkout.js
+++ src/blocks/checkout/checkout.js
@@ -7,19 +7,20 @@ if ($('.js-slider-checkout').length) {
     slidesToShow: 2,
     centerPadding: '0',
     centerMode: true,
+    prevArrow: $('.btn-slider--prev'),
+    nextArrow: $('.btn-slider--next'),
     responsive: [
       {
         breakpoint: 767,
         settings: {
-          arrows: true,
-          prevArrow: $('.btn-slider--prev'),
-          nextArrow: $('.btn-slider--next')
+          arrows: true
         }
       },
       {
         breakpoint: 991,
         settings: {
           slidesToShow: 4,
+          arrows: true,
           centerPadding: '32px' // ширина выступающей картинки (20px) + половина отступа между слайдами (12px)
         }
       },
@@ -27,6 +28,7 @@ if ($('.js-slider-checkout').length) {
         breakpoint: 1199,
         settings: {
           slidesToShow: 4,
+          arrows: true,
           centerPadding: '59px'
         }
       },
@@ -34,6 +36,7 @@ if ($('.js-slider-checkout').length) {
         breakpoint: 1799,
         settings: {
           slidesToShow: 5,
+          arrows: true,
           centerPadding: '120px' // ширина выступающей картинки (100px) + половина отступа между слайдами (20px)
         }
       },

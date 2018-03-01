c63e411f635243e41a75267ece9162d39216c08c
pashkes <pashkes@gmail.com>
2018-03-01 23:26:27 +0200

    fixed sliders product

--- src/blocks/checkout/checkout.js
+++ src/blocks/checkout/checkout.js
@@ -1,48 +1,50 @@
-if ($('.js-slider-checkout').length) {
-  $('.js-slider-checkout').slick({
-    mobileFirst: true,
-    dots: false,
-    arrows: false,
-    speed: 300,
-    slidesToShow: 2,
-    centerPadding: '0',
-    centerMode: true,
-    prevArrow: $('.js-slider-product-prev'),
-    nextArrow: $('.js-slider-product-next'),
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          arrows: true,
-          rows: 2
-        }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 4,
-          arrows: true,
-          rows: 1,
-          centerPadding: '32px' // ширина выступающей картинки (20px) + половина отступа между слайдами (12px)
-        }
-      },
-      {
-        breakpoint: 1199,
-        settings: {
-          slidesToShow: 4,
-          arrows: true,
-          centerPadding: '59px'
-        }
-      },
-      {
-        breakpoint: 1799,
-        settings: {
-          slidesToShow: 5,
-          arrows: true,
-          centerPadding: '113px' // ширина выступающей картинки (100px) + половина отступа между слайдами (20px)
-        }
-      }
+$(function () {
+	if ($('.js-slider-checkout').length) {
+		$('.js-slider-checkout').slick({
+			slidesToShow: 5,
+			arrows: true,
+			centerPadding: '113px', // ширина выступающей картинки (100px) + половина отступа между слайдами (20px)
+			prevArrow: $('.js-slider-product-prev'),
+			nextArrow: $('.js-slider-product-next'),
+			dots: false,
+			speed: 300,
+			centerMode: true,
 
-    ]
-  });
-}
+			responsive: [
+				{
+					breakpoint: 767,
+					settings: {
+						arrows: false,
+						slidesToShow: 2,
+						variableWidth: true,
+						centerMode: false,
+						rows: 1
+					}
+				},
+				{
+					breakpoint: 991,
+					settings: {
+						slidesToShow: 2,
+						rows: 2,
+						centerPadding: '0'
+					}
+				},
+				{
+					breakpoint: 1199,
+					settings: {
+						slidesToShow: 4,
+						centerPadding: '32px' // ширина выступающей картинки (20px) + половина отступа между слайдами (12px)
+					}
+				},
+				{
+					breakpoint: 1799,
+					settings: {
+						centerPadding: '59px'
+					}
+				}
+
+			]
+		});
+	}
+
+});
